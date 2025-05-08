c41ca20598c62893cd889d1bdf8cdf9030d971dc
ariniariyaniputri <ariniariyani35@gmail.com>
2025-05-08 13:56:08 +0700

    Add files via upload

---
+++ komentar.py
@@ -0,0 +1,26 @@
+#Ini adalah komentar
+# Tulisan ini tidak akan dieksekusi
+
+#komentar dengan tanda pagar hanya bisa digunakan
+#untuk
+#satu
+#baris
+
+"""
+Penulisan Komentar lebih dari satu baris yaitu
+dengan menggunakan kutip dua 3 kali dan
+ditutup dengan kutip dua 3 kali juga
+"""
+print("hello arin") #ini juga komentar
+"""
+print("hello arin") # ini komentar
+"""
+#print("Welcome")
+
+# komentar bisa berisi spesial karakter !@#$%^&*(),./;'[]\
+
+#mencetak nama
+print("arin")
+
+#mencetak angka/integer
+print(15052008)
