86c8c0ad4dc460067f77e68b7005d8fa9ebbc7cb
ariniariyaniputri <ariniariyani35@gmail.com>
2025-05-19 09:47:28 +0700

    tipedata

---
+++ tipedata.py
@@ -0,0 +1,36 @@
+#tipe data Boolean
+print(True)
+
+#tipe data String
+print("Ayo belajar Python")
+print('Belajar Python Sangat Mudah')
+
+#tipe data Integer
+print(-20)
+
+#tipe data Float
+print(3.14)
+
+#tipe data Hexadecimal
+print("Bentuk hexadecimal dari 23 adalah " + hex(23))
+
+print("nama saya : " + "sasa")
+
+#tipe data Complex
+print(5j)
+
+#tipe data List
+print([1,2,3,4,5])
+print(["satu", "dua", "tiga"])
+
+#tipe data Tuple
+print((1,2,3,4,5))
+print(("satu", "dua", "tiga"))
+
+#tipe data Dictionary
+print({"nama":"ariyani", 'umur':16})
+
+#tipe data Dictionary dimasukan ke dalam variabel biodata
+biodata = {"nama":"arin", 'umur':16} #proses inisialisasi variabel biodata
+print(biodata) #proses pencetakan variabel biodata yang berisi tipe data Dictionary
+print(type(biodata)) #fungsi untuk mengecek jenis tipe data. akan tampil <class 'dict'> yang berarti dict adalah tipe data dictionary
