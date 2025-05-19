6d12e2190305a20e98c6740a48d0625320291b07
ariniariyaniputri <ariniariyani35@gmail.com>
2025-05-19 09:47:56 +0700

    string

---
+++ string.py
@@ -0,0 +1,23 @@
+# Contoh 1
+print("contoh 1")
+message = 'Hello'
+print('Sebelum Update = ', message)
+
+message = message + ' Arini '
+print('Sesudah Update = ', message)
+print("\n")
+
+# Contoh 2
+print("contoh 2")
+nama = 'gele gila'
+print('sebelum =', nama)
+
+nama_lengkap = nama[:5] + ' kencana ' + nama[6:]
+print('sesudah =', nama_lengkap)
+print("\n")
+
+# Contoh 3
+print("contoh 3")
+x = nama [6:] + " " + nama [:5]
+print('kombinasi baru =', x) 
+print("\n")
